16032bfd30f61ea6cfd300af6749ae487539355b
Aditya Sinha <sinhadityaforever@gmail.com>
2022-05-17 11:42:45 +0530

    Deleted secrets

--- src/secrets.ts
+++
@@ -1,8 +0,0 @@
-export const secret = {
-	clientId:
-		'956270240695-feis0172u7uobr1p220qa5lermsu9lom.apps.googleusercontent.com',
-	apiKey: 'AIzaSyAhELxTMAr6ofcngVVMs-012cCAkHBz0kQ',
-	projectNumber: '956270240695',
-	discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
-	scopes: 'https://www.googleapis.com/auth/drive.metadata.readonly'
-};
